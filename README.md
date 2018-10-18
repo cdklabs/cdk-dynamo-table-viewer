249df3d783622d29cdceb205251b7cb785f088bf
Elad Ben-Israel <benisrae@amazon.com>
2018-10-18 17:10:14 +0300

    Initial functional version

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # cdk-dynamo-table-viewer
-A CDK construct which exposes an endpoint with the contents of a DynamoDB table
+
+An AWS CDK construct which exposes a public HTTP endpoint which displays an HTML
+page with the contents of a DynamoDB table in your stack.
+
+__SECURITY NOTE__: this construct was built for demonstration purposes and
+using it in production is probably a really bad idea. It exposes the entire
+contents of a DynamoDB table in your account to the general public.
+
+## Usage
+
+Install via npm:
+
+```shell
+$ npm i cdk-dynamo-table-viewer
+```
+
+Add to your CDK stack:
+
+```ts
+import { TableViewer } from 'cdk-dynamo-table-viewer'
+
+const viewer = new TableViewer(this, 'CookiesViewer', {
+  table: cookiesTable,
+  title: 'Cookie Sales', // optional
+  sort: '-sales'         // optional ("-" denotes descending order)
+});
+```
+
+Notes:
+
+- The endpoint will be available (as an deploy-time value) under `viewer.endpoint`.
+  It will also be exported as a stack output.
+- Paging is not supported. This means that only the first 1MB of items will be
+  displayed (again, this is a demo...)
+
+## License
+
+Apache 2.0
---
+++ lib/table-viewer.ts
@@ -0,0 +1,56 @@
+import cdk = require('@aws-cdk/cdk');
+import apigw = require('@aws-cdk/aws-apigateway');
+import lambda = require('@aws-cdk/aws-lambda');
+import dynamodb = require('@aws-cdk/aws-dynamodb');
+import path = require('path');
+
+export interface TableViewerProps {
+  /**
+   * The DynamoDB table to view. Note that all contents of this table will be
+   * visible to the public.
+   */
+  table: dynamodb.Table;
+
+  /**
+   * The web page title.
+   * @default No title
+   */
+  title?: string;
+
+  /**
+   * Name of the column to sort by, prefix with "-" for descending order.
+   * @default No sort
+   */
+  sortBy?: string;
+}
+
+/**
+ * Installs an endpoint in your stack that allows users to view the contents
+ * of a DynamoDB table through their browser.
+ */
+export class TableViewer extends cdk.Construct {
+
+  public readonly endpoint: string;
+
+  constructor(parent: cdk.Construct, id: string, props: TableViewerProps) {
+    super(parent, id);
+
+    const handler = new lambda.Function(this, 'Rendered', {
+      code: lambda.Code.directory(path.join(__dirname, 'lambda')),
+      runtime: lambda.Runtime.NodeJS810,
+      handler: 'index.handler',
+      environment: {
+        TABLE_NAME: props.table.tableName,
+        TITLE: props.title,
+        SORT_BY: props.sortBy
+      }
+    });
+
+    props.table.grantReadData(handler.role);
+
+    const home = new apigw.LambdaRestApi(this, 'ViewerEndpoint', { handler, proxyPath: '/' });
+    home.root.addMethod('ANY');
+
+    this.endpoint = home.url;
+  }
+}
