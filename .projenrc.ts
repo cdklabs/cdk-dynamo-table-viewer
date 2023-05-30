8d39d481c31fee206d92243edf2d08f97a2c113a
Kaizen Conroy <zen.conroy@gmail.com>
2023-05-30 11:03:41 -0400

    chore: update to projenrcTs

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,20 +1,28 @@
-const { CdklabsConstructLibrary } = require('cdklabs-projen-project-types');
+import { CdklabsConstructLibrary } from 'cdklabs-projen-project-types';
 
 const project = new CdklabsConstructLibrary({
   defaultReleaseBranch: 'master',
+  projenrcTs: true,
   enablePRAutoMerge: true,
   private: false,
   repositoryUrl: 'https://github.com/cdklabs/cdk-dynamo-table-viewer.git',
   name: 'cdk-dynamo-table-viewer',
   description: 'An AWS CDK construct which exposes an endpoint with the contents of a DynamoDB table',
 
-  projenUpgradeSecret: 'PROJEN_GITHUB_TOKEN',
+  author: 'Amazon Web Services',
+  authorAddress: 'aws-cdk-dev@amazon.com',
 
   cdkVersion: '2.60.0',
   minNodeVersion: '16.14.0',
   workflowNodeVersion: '16.x',
 
-  devDeps: ['ts-node', 'aws-cdk-lib', 'constructs', '@aws-sdk/client-dynamodb', 'cdklabs-projen-project-types'],
+  devDeps: [
+    'ts-node@^10.8.1',
+    'aws-cdk-lib',
+    'constructs',
+    '@aws-sdk/client-dynamodb',
+    'cdklabs-projen-project-types',
+  ],
   peerDeps: ['aws-cdk-lib', 'constructs'],
 
   catalog: {
@@ -43,14 +51,9 @@ const project = new CdklabsConstructLibrary({
     packageName: 'dynamotableviewer',
   },
 
-  autoApproveOptions: {
-    allowedUsernames: ['cdklabs-automation'],
-    secret: 'GITHUB_TOKEN',
-  },
   depsUpgradeOptions: {
     exclude: ['aws-cdk-lib', 'constructs'],
   },
-  autoApproveUpgrades: true,
 });
 
 project.synth();
